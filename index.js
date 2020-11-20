be293c17d48cb7d94f1f8463664d01a8246d58c0
Tororoi <cantwell.tom@gmail.com>
2020-11-20 11:34:42 -0500

    cleaned out image-based undo

--- index.js
+++ index.js
@@ -14,18 +14,8 @@ let rect;
     onScreenCVS.height = baseDimension;
 
 //Create history stacks for the undo functionality
-//Image based
-// let undoStack = [onScreenCVS.toDataURL()];
-//Action based
 let undoStack = [];
 let redoStack = [];
-function getTopImage() {
-    return undoStack[undoStack.length-1]
-}
-
-//Create an Image with a default source of the existing onscreen canvas
-let img = new Image;
-let source = getTopImage();
 
 //Create an offscreen canvas. This is where we will actually be drawing, in order to keep the image consistent and free of distortions.
 let offScreenCVS = document.createElement('canvas');
@@ -33,6 +23,10 @@ let offScreenCTX = offScreenCVS.getContext("2d");
 //Set the dimensions of the drawing canvas
   offScreenCVS.width = 32;
   offScreenCVS.height = 32;
+
+//Create an Image with a default source of the existing onscreen canvas
+let img = new Image;
+let source = offScreenCVS.toDataURL();
 
 //Add event listeners for the mouse moving, downclick, and upclick
 onScreenCVS.addEventListener('mousemove', handleMouseMove);
@@ -43,36 +37,28 @@ onScreenCVS.addEventListener('mouseup', handleMouseUp);
 undoBtn.addEventListener('click', handleUndo)
 redoBtn.addEventListener('click', handleRedo)
 
-
 //We only want the mouse to move if the mouse is down, so we need a variable to disable drawing while the mouse is not clicked.
 let clicked = false;
 
+//Base settings
+brushColor = "red";
+
 function handleMouseMove(e) {
     if (clicked) {
-        //Action-based
         actionDraw(e);
-        //Image-based
-        // draw(e)
     }
 }
 
 function handleMouseDown(e) {
     clicked = true;
-    //Action-based
     actionDraw(e);
-    //Image-based
-    // draw(e);
 }
 
 function handleMouseUp() {
     clicked = false;
-    //Action-based
+    //add to undo stack
     undoStack.push(points);
     points = [];
-    //Image-based
-    //Push the image to the history
-    // undoStack.push(source)
-
     //Reset redostack
     redoStack = [];
 }
@@ -80,14 +66,12 @@ function handleMouseUp() {
 function handleUndo() {
     if (undoStack.length>0) {
         actionUndoRedo(redoStack, undoStack);
-        // undoRedo(redoStack, undoStack);
     }
 }
 
 function handleRedo() {
     if (redoStack.length>=1) {
         actionUndoRedo(undoStack, redoStack);
-        // undoRedo(undoStack, redoStack);
     }
 }
 
@@ -101,7 +85,7 @@ function actionDraw(e) {
     let mouseX = Math.floor(e.offsetX/ratio);
     let mouseY = Math.floor(e.offsetY/ratio);
     // extend the polyline
-    offScreenCTX.fillStyle = "red";
+    offScreenCTX.fillStyle = brushColor;
     offScreenCTX.fillRect(mouseX,mouseY,1,1);
 
     if (lastX !== mouseX || lastY !== mouseY) {
@@ -109,7 +93,7 @@ function actionDraw(e) {
             x: mouseX,
             y: mouseY,
             // size: brushSize,
-            // color: brushColor,
+            color: brushColor,
             // mode: "draw"
         });
         source = offScreenCVS.toDataURL();
@@ -120,6 +104,8 @@ function actionDraw(e) {
     lastX = mouseX;
     lastY = mouseY;
 }
+
+//Helper functions
 
 function actionUndoRedo(pushStack,popStack) {
     pushStack.push(popStack.pop());
@@ -132,21 +118,10 @@ function actionUndoRedo(pushStack,popStack) {
 function redrawPoints() {
     undoStack.forEach(s => {
         s.forEach(p => {
-            offScreenCTX.fillStyle = "red";
+            offScreenCTX.fillStyle = p.color;
             offScreenCTX.fillRect(p.x,p.y,1,1);
         })
     })
-}
-//Helper functions
-
-//Draw a single pixel on the canvas. Get the ratio of the difference in size of the on and offscreen canvases to calculate where to draw on the offscreen canvas based on the coordinates of clicking on the onscreen canvas.
-function draw(e) {
-    let ratio = onScreenCVS.width/offScreenCVS.width;
-    offScreenCTX.fillStyle = "red";
-    offScreenCTX.fillRect(Math.floor(e.offsetX/ratio),Math.floor(e.offsetY/ratio),1,1);
-    //Set the source of the image to the offscreen canvas
-    source = offScreenCVS.toDataURL();
-    renderImage();
 }
 
 //Once the image is loaded, draw the image onto the onscreen canvas.
@@ -160,16 +135,6 @@ function renderImage() {
       onScreenCTX.imageSmoothingEnabled = false;
       onScreenCTX.drawImage(img,0,0,onScreenCVS.width,onScreenCVS.height)
     }
-}
-
-//Undo or redo an action
-function undoRedo(pushStack,popStack) {
-    pushStack.push(popStack.pop());
-    source = getTopImage();
-    img.src = source;
-    offScreenCTX.clearRect(0,0,offScreenCVS.width,offScreenCVS.height);
-    offScreenCTX.drawImage(img,0,0,offScreenCVS.width,offScreenCVS.height);
-    renderImage();
 }
 
 //Get the size of the parentNode which is subject to flexbox. Fit the square by making sure the dimensions are based on the smaller of the width and height.
