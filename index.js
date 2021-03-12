1a1315cb9a2efc8cb9e6a75f9c9761712b4ccecd
Tororoi <cantwell.tom@gmail.com>
2021-03-12 10:21:17 -0500

    improved bezier algorithm, flat curves cause problems

--- index.js
+++ index.js
@@ -1044,7 +1044,7 @@ function actionCurve(x1, y1, x2, y2, x3, y3, stepNum, currentColor, ctx, current
         //no rounding
         return p3 + Math.pow((1 - t), 2) * (p1 - p3) + Math.pow(t, 2) * (p2 - p3);
     }
-    let tNum = 320;
+    let tNum = 32;
     lastXt = x1;
     lastYt = y1;
 
@@ -1083,133 +1083,92 @@ function actionCurve(x1, y1, x2, y2, x3, y3, stepNum, currentColor, ctx, current
         return { xdiff: xdiff, ydiff: ydiff };
     }
 
-    function renderCurve(tMax, controlX, controlY) {
-        let xNext, yNext;
-        for (let i = 0; i < tMax / 2; i++) {
-            let j = tMax - i;
-            let truext = pt(x1, x2, controlX, i / tMax);
-            let trueyt = pt(y1, y2, controlY, i / tMax);
-
-            // //derivatives
-            let dxt = dpt(x1, x2, controlX, i / tMax);
-            let dyt = dpt(y1, y2, controlY, i / tMax);
-            let ddxt = ddpt(x1, x2, controlX);
-            let ddyt = ddpt(y1, y2, controlY);
-
-            let sign = Math.sign(denom(dxt, dyt, ddxt, ddyt));
-            let rad = radius(dxt, dyt, ddxt, ddyt, i);
-            let s = speed(dxt, dyt);
-            let circlex = truext - sign * (dyt / s) * rad;
-            let circley = trueyt - sign * (-dxt / s) * rad;
-
+    function renderCurve(controlX, controlY) {
+        function plot(x, y) {
             //rounded values
-            let xt = Math.floor(truext);
-            let yt = Math.floor(trueyt);
+            let xt = Math.floor(x);
+            let yt = Math.floor(y);
 
-            if (i !== 0 && (xt !== xNext || yt !== yNext)) {
-                //skip this t value
-                continue;
-            }
-
-
-            // // console.log(incT(dxt, dyt))
-
-            //BUG: calculation breaks down for sharp curves due to small size of tangent circle
-            //maybe run curve rendering from t=1 simultaneously so it meets in the middle at t=0.5
-            let m1 = Math.abs(Math.sqrt(Math.pow(xt + 1.5 - circlex, 2) + Math.pow(yt + 0.5 - circley, 2)) - rad);
-            let m2 = Math.abs(Math.sqrt(Math.pow(xt + 1.5 - circlex, 2) + Math.pow(yt + 1.5 - circley, 2)) - rad);
-            let m3 = Math.abs(Math.sqrt(Math.pow(xt + 0.5 - circlex, 2) + Math.pow(yt + 1.5 - circley, 2)) - rad);
-            let m4 = Math.abs(Math.sqrt(Math.pow(xt - 0.5 - circlex, 2) + Math.pow(yt + 1.5 - circley, 2)) - rad);
-            let m5 = Math.abs(Math.sqrt(Math.pow(xt - 0.5 - circlex, 2) + Math.pow(yt + 0.5 - circley, 2)) - rad);
-            let m6 = Math.abs(Math.sqrt(Math.pow(xt - 0.5 - circlex, 2) + Math.pow(yt - 0.5 - circley, 2)) - rad);
-            let m7 = Math.abs(Math.sqrt(Math.pow(xt + 0.5 - circlex, 2) + Math.pow(yt - 0.5 - circley, 2)) - rad);
-            let m8 = Math.abs(Math.sqrt(Math.pow(xt + 1.5 - circlex, 2) + Math.pow(yt - 0.5 - circley, 2)) - rad);
-
-            let direction = [];
-
-            switch (true) {
-                case (Math.sign(dxt) === 1 && Math.sign(dyt) === 1):
-                    //Q1 (lower right)
-                    direction.push(m1, m2, m3);
-                    break;
-                case (Math.sign(dxt) === -1 && Math.sign(dyt) === 1):
-                    //Q2
-                    direction.push(m3, m4, m5);
-                    break;
-                case (Math.sign(dxt) === -1 && Math.sign(dyt) === -1):
-                    //Q3
-                    direction.push(m5, m6, m7);
-                    break;
-                case (Math.sign(dxt) === 1 && Math.sign(dyt) === -1):
-                    //Q4
-                    direction.push(m7, m8, m1);
-                    break;
-                default:
-                    continue;
-
-            }
-
-            direction.sort();
-
-
-            switch (direction[0]) {
-                case m1:
-                    xNext = xt + 1;
-                    yNext = yt;
-                    break;
-                case m2:
-                    xNext = xt + 1;
-                    yNext = yt + 1;
-                    break;
-                case m3:
-                    xNext = xt;
-                    yNext = yt + 1;
-                    break;
-                case m4:
-                    xNext = xt - 1;
-                    yNext = yt + 1;
-                    break;
-                case m5:
-                    xNext = xt - 1;
-                    yNext = yt;
-                    break;
-                case m6:
-                    xNext = xt - 1;
-                    yNext = yt - 1;
-                    break;
-                case m7:
-                    xNext = xt;
-                    yNext = yt - 1;
-                    break;
-                case m8:
-                    xNext = xt + 1;
-                    yNext = yt - 1;
-                    break;
-                default:
-                //
-            }
-
-            // if (i === 10) {
-            //     console.log("x:",truext,"y:",trueyt);
-            //     console.log("slope:",dyt/dxt);
-            //     console.log("radius:",rad)
-            //     console.log("m1:",m1)
-            //     console.log("m2:",m2)
-            //     console.log("m3:",m3)
-            //     console.log([m3,m2,m1].sort())
-            // }
-
-            // i === 6 ? ctx.fillStyle = "blue" : ctx.fillStyle = currentColor.color;
-            // if (i === 10) {
-            //     actionLine(xt, yt, Math.floor(circlex), Math.floor(circley), currentColor, ctx, currentMode, scale);
-            // }
-
-            // actionLine(lastXt, lastYt, xt, yt, currentColor, ctx, currentMode, scale);
-
+            //plot starting coordinates
             if (stepNum === 2 || stepNum === 3) {
                 onScreenCTX.fillRect(xt * state.ratio / zoom, yt * state.ratio / zoom, scale, scale);
             } else if (stepNum === 4) {
                 ctx.fillRect(xt, yt, scale, scale)
+            }
+        }
+
+        function assert(condition, message) {
+            if (!condition) {
+                throw new Error(message || "Assertion failed");
+            }
+        }
+
+        plotQuadBezier(x1, y1, controlX, controlY, x2, y2);
+
+        function plotQuadBezier(x0, y0, x1, y1, x2, y2) { /* plot any quadratic Bezier curve */
+            let x = x0 - x1, y = y0 - y1;
+            let t = x0 - 2 * x1 + x2, r;
+            if (x * (x2 - x1) > 0) { /* horizontal cut at P4? */
+                if (y * (y2 - y1) > 0) /* vertical cut at P6 too? */
+                    if (Math.abs((y0 - 2 * y1 + y2) / t * x) > Math.abs(y)) { /* which first? */
+                        x0 = x2; x2 = x + x1; y0 = y2; y2 = y + y1; /* swap points */
+                    } /* now horizontal cut at P4 comes first */
+                t = (x0 - x1) / t;
+                r = (1 - t) * ((1 - t) * y0 + 2.0 * t * y1) + t * t * y2; /* By(t=P4) */
+                t = (x0 * x2 - x1 * x1) * t / (x0 - x1); /* gradient dP4/dx=0 */
+                x = Math.floor(t + 0.5); y = Math.floor(r + 0.5);
+                r = (y1 - y0) * (t - x0) / (x1 - x0) + y0; /* intersect P3 | P0 P1 */
+                plotQuadBezierSeg(x0, y0, x, Math.floor(r + 0.5), x, y);
+                r = (y1 - y2) * (t - x2) / (x1 - x2) + y2; /* intersect P4 | P1 P2 */
+                x0 = x1 = x; y0 = y; y1 = Math.floor(r + 0.5); /* P0 = P4, P1 = P8 */
+            }
+            if ((y0 - y1) * (y2 - y1) > 0) { /* vertical cut at P6? */
+                t = y0 - 2 * y1 + y2; t = (y0 - y1) / t;
+                r = (1 - t) * ((1 - t) * x0 + 2.0 * t * x1) + t * t * x2; /* Bx(t=P6) */
+                t = (y0 * y2 - y1 * y1) * t / (y0 - y1); /* gradient dP6/dy=0 */
+                x = Math.floor(r + 0.5); y = Math.floor(t + 0.5);
+                r = (x1 - x0) * (t - y0) / (y1 - y0) + x0; /* intersect P6 | P0 P1 */
+                plotQuadBezierSeg(x0, y0, Math.floor(r + 0.5), y, x, y);
+                r = (x1 - x2) * (t - y2) / (y1 - y2) + x2; /* intersect P7 | P1 P2 */
+                x0 = x; x1 = Math.floor(r + 0.5); y0 = y1 = y; /* P0 = P6, P1 = P7 */
+            }
+            plotQuadBezierSeg(x0, y0, x1, y1, x2, y2); /* remaining part */
+        }
+
+        //bresenham's algorithm for bezier limited to gradients without sign change.
+        function plotQuadBezierSeg(x0, y0, x1, y1, x2, y2) {
+            let sx = x2 - x1, sy = y2 - y1;
+            let xx = x0 - x1, yy = y0 - y1, xy;         /* relative values for checks */
+            let dx, dy, err, cur = xx * sy - yy * sx;                    /* curvature */
+
+            assert(xx * sx <= 0 && yy * sy <= 0, "sign of gradient changed");  /* sign of gradient must not change */
+
+            if (sx * sx + sy * sy > xx * xx + yy * yy) { /* begin with longer part */
+                x2 = x0; x0 = sx + x1; y2 = y0; y0 = sy + y1; cur = -cur;  /* swap P0 P2 */
+            }
+            if (cur != 0) {                                    /* no straight line */
+                xx += sx; xx *= sx = x0 < x2 ? 1 : -1;           /* x step direction */
+                yy += sy; yy *= sy = y0 < y2 ? 1 : -1;           /* y step direction */
+                xy = 2 * xx * yy; xx *= xx; yy *= yy;          /* differences 2nd degree */
+                if (cur * sx * sy < 0) {                           /* negated curvature? */
+                    xx = -xx; yy = -yy; xy = -xy; cur = -cur;
+                }
+                dx = 4.0 * sy * cur * (x1 - x0) + xx - xy;             /* differences 1st degree */
+                dy = 4.0 * sx * cur * (y0 - y1) + yy - xy;
+                xx += xx; yy += yy; err = dx + dy + xy;                /* error 1st step */
+                while (dy < dx) { /* gradient negates -> algorithm fails */
+                    plot(x0, y0);                                     /* plot curve */
+                    if (x0 == x2 && y0 == y2) return;  /* last pixel -> curve finished */
+                    y1 = 2 * err < dx;                  /* save value for test of y step */
+                    if (2 * err > dy) { x0 += sx; dx -= xy; err += dy += yy; } /* x step */
+                    if (y1) { y0 += sy; dy -= xy; err += dx += xx; } /* y step */
+                }
+            }
+            /* plot remaining part to end */
+            if (stepNum === 2 || stepNum === 3) {
+                actionLine(x0 * state.ratio / zoom, y0 * state.ratio / zoom, x2 * state.ratio / zoom, y2 * state.ratio / zoom, state.brushColor, onScreenCTX, state.mode, state.ratio / zoom);
+            } else if (stepNum === 4) {
+                actionLine(x0, y0, x2, y2, state.brushColor, ctx, state.mode);
             }
         }
     }
@@ -1251,6 +1210,8 @@ function actionCurve(x1, y1, x2, y2, x3, y3, stepNum, currentColor, ctx, current
 
             //BUG: calculation breaks down for sharp curves due to small size of tangent circle
             //running curve rendering from t=1 simultaneously so it meets in the middle at t=0.5 is not enough
+            //Needs a better way to calculate next pixel for sharp curves
+            //bresenham's algorithm using the circle to estimate. 
             let m1 = Math.abs(Math.sqrt(Math.pow(xt + 1.5 - circlex, 2) + Math.pow(yt + 0.5 - circley, 2)) - rad);
             let m2 = Math.abs(Math.sqrt(Math.pow(xt + 1.5 - circlex, 2) + Math.pow(yt + 1.5 - circley, 2)) - rad);
             let m3 = Math.abs(Math.sqrt(Math.pow(xt + 0.5 - circlex, 2) + Math.pow(yt + 1.5 - circley, 2)) - rad);
@@ -1355,7 +1316,7 @@ function actionCurve(x1, y1, x2, y2, x3, y3, stepNum, currentColor, ctx, current
             if (stepNum === 2 || stepNum === 3) {
                 onScreenCTX.fillRect(xt * state.ratio / zoom, yt * state.ratio / zoom, scale, scale);
             } else if (stepNum === 4) {
-                ctx.fillRect(xt, yt, scale, scale)
+                ctx.fillRect(xt, yt, scale, scale);
             }
         }
         let xlnext, ylnext;
@@ -1508,8 +1469,8 @@ function actionCurve(x1, y1, x2, y2, x3, y3, stepNum, currentColor, ctx, current
         //onscreen preview curve
         // bezier curve
         // tNum = Math.abs(x1 - x2) > Math.abs(y1 - y2) ? Math.floor(Math.abs(x1 - x2) * 20) : Math.floor(Math.abs(y1 - y2) * 20);
-        // renderCurve(tNum, state.mox, state.moy);
-        renderCurve2(state.mox, state.moy);
+        renderCurve(state.mox, state.moy);
+        // renderCurve2(state.mox, state.moy);
         // for (let i = 0; i < tNum; i++) {
         //     let truext = pt(x1, x2, state.mox, i / tNum);
         //     let trueyt = pt(y1, y2, state.moy, i / tNum);
@@ -1530,8 +1491,8 @@ function actionCurve(x1, y1, x2, y2, x3, y3, stepNum, currentColor, ctx, current
         // tNum = Math.floor(Math.sqrt(Math.pow(x1-x2,2)+Math.pow(y1-y2,2)));
         // tNum = Math.abs(x1 - x2) > Math.abs(y1 - y2) ? Math.floor(Math.abs(x1 - x2) * 20) : Math.floor(Math.abs(y1 - y2) * 20);
         // console.log("tNum:", tNum)
-        // renderCurve(tNum, x3, y3);
-        renderCurve2(x3, y3);
+        renderCurve(x3, y3);
+        // renderCurve2(x3, y3);
 
         //render drawing
         source = offScreenCVS.toDataURL();
