6abaa46d0fe3ec848944a86f252b8cad423ab56c
Tororoi <cantwell.tom@gmail.com>
2023-12-18 17:19:13 -0800

    fixed ellipse subpixel option

--- src/Controls/events.js
+++ src/Controls/events.js
@@ -158,7 +158,7 @@ function handlePointerMove(e) {
   setCoordinates(e)
   let cursorMoved =
     state.previousX !== state.cursorX || state.previousY !== state.cursorY
-  if (state.tool.options.useSubPixels?.active && !cursorMoved) {
+  if (state.tool.options.useSubpixels?.active && !cursorMoved) {
     cursorMoved =
       canvas.previousSubPixelX !== canvas.subPixelX ||
       canvas.previousSubPixelY !== canvas.subPixelY
